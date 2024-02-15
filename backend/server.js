98cf8609274e608716ad50fa6d50217e6fb62740
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2024-02-14 18:18:36 -0700

    add routing

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,25 @@
 const express = require("express");
-
 const app = express();
-
 const port = 4000;
 
+//get all tasks
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Send all tasks");
 });
 
-app.get("/:id", (req, res) => {
-  res.send(`Hello ${req.params.id}`);
+//edit task
+app.patch("/:id", (req, res) => {
+  res.send(`Editing task number ${req.params.id}`);
+});
+
+//make new task
+app.post("/", (req, res) => {
+  res.send("Creating a new task");
+});
+
+//delete task
+app.delete("/:id", (req, res) => {
+  res.send(`Deleting task ${req.params.id}`);
 });
 
 app.listen(port, () => {
