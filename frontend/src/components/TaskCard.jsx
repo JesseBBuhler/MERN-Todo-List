4cc763128b23f721146af1bf80c53107eb70bbbf
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2024-02-19 10:18:09 -0700

    style check boxes

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -3,9 +3,9 @@ const TaskCard = (props) => {
   return (
     <div className="taskCard">
       <h2>{props.data.task}</h2>
-      <div
-        className={`checkbox ${props.data.completed ? "checked" : ""}`}
-      ></div>
+      <div className={`checkbox ${props.data.completed ? "checked" : ""}`}>
+        {props.data.completed ? <i class="fa-solid fa-check fa-xl"></i> : ""}
+      </div>
     </div>
   );
 };
