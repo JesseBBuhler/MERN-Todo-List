bc7ac64647e343a92b0eef24d3767d0b5f9ae37b
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2024-02-19 11:19:38 -0700

    add delete button

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -1,10 +1,31 @@
 const TaskCard = (props) => {
-  console.log(props.data.task, props.data.completed);
+  const toggleTaskCompletion = async () => {
+    const response = await fetch(`api/tasks/${props.data._id}`, {
+      method: "PATCH",
+    });
+    const json = await response.json();
+    alert(json);
+  };
+
+  const deleteTask = async () => {
+    const response = await fetch(`api/tasks/${props.data._id}`, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+    alert(json);
+  };
+
   return (
     <div className="taskCard">
-      <h2>{props.data.task}</h2>
-      <div className={`checkbox ${props.data.completed ? "checked" : ""}`}>
+      <div
+        onClick={toggleTaskCompletion}
+        className={`checkbox ${props.data.completed ? "checked" : ""}`}
+      >
         {props.data.completed ? <i class="fa-solid fa-check fa-xl"></i> : ""}
+      </div>
+      <h2>{props.data.task}</h2>
+      <div onClick={deleteTask} className="deleteButton">
+        <i class="fa-regular fa-trash-can fa-xl lower"></i>
       </div>
     </div>
   );
